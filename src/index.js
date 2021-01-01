b5800085f348f0dccc5e92db8ddf60e163a694a5
GerdaAm <63974776+GerdaAm@users.noreply.github.com>
2021-01-01 16:37:25 +0200

    Add H1 with unordered list

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My pets' names are: </h1>
+    <ul>
+      <li>Katryn</li>
+      <li>Oscar</li>
+      <li>Wallace</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
